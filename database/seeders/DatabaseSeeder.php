1766383931c8e5f5994f859d7de9aea895d28ed2
brito101 <rodrigobrito101@hotmail.com>
2025-01-21 20:01:17 -0300

    mod: change header on web layer

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,19 +4,21 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder {
+class DatabaseSeeder extends Seeder
+{
 
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run() {
+    public function run()
+    {
         $this->call([AdminTableUserSeeder::class]);
         $this->call([RolesTableSeeder::class]);
         $this->call([PermissionsTableSeeder::class]);
         $this->call([ModelHasRoleTableSeeder::class]);
         $this->call([RoleHasPermissionsTableSeeder::class]);
+        $this->call([TermsTableSeeder::class]);
     }
-
 }
